091006fbb85b53de022634cb17b83572d910a92b
rafaelangical <rsnrafael@outlook.com>
2017-05-23 13:30:03 -0300

    CSS and basic updates

--- src/Components/Sections.js
+++ src/Components/Sections.js
@@ -7,25 +7,25 @@ export default function Sections(){
       <div class="sections">
         <section id="home" class="content">
           <article class="box home">
-          
+            <h2>Home</h2>
           </article>
         </section>
         
         <section id="company" class="content">
           <article class="box company">
-            
+            <h2>Company</h2>
           </article>
         </section>
         
         <section id="service" class="content">
           <article class="box service">
-            
+            <h2>Serviços</h2>
           </article>
         </section>
         
         <section id="contact" class="content">
           <article class="box contact">
-            
+            <h2>Contatos</h2>
           </article>
         </section>
       </div>
